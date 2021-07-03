92b8cfb415ebbd2f7a1f46a0cc8bbfb9de1545f1
Ariel Pena <olafpena@gmail.com>
2021-07-03 16:20:01 -0500

    Added selected option

--- tu-recibo/src/main/java/org/mexico/tgm/controller/ReciboController.java
+++ tu-recibo/src/main/java/org/mexico/tgm/controller/ReciboController.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.mexico.tgm.model.CustomLdapUserDetails;
 import org.mexico.tgm.model.Ruta;
 import org.slf4j.Logger;
@@ -16,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jcifs.smb1.smb1.NtlmPasswordAuthentication;
 import jcifs.smb1.smb1.SmbFile;
@@ -24,19 +23,23 @@ import jcifs.smb1.smb1.SmbFile;
 public class ReciboController {
 
 	private static Logger logger = LoggerFactory.getLogger(ReciboController.class);
+	
+	private String annio;
 
 	@GetMapping("/recibo")
-	public String recibo(Model model, @PathParam("annio") String annio) {
+	public String recibo(Model model, @RequestParam(name = "annio",required = false) String annio) {
 		if(annio==null) {
 			Calendar calendar = Calendar.getInstance();
-			 annio = String.valueOf(calendar.get(Calendar.YEAR));			
+			this.annio = String.valueOf(calendar.get(Calendar.YEAR));			
+		}else {
+			this.annio = annio;
 		}
-
 		
 		CustomLdapUserDetails userDetails = (CustomLdapUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		model.addAttribute("userName", userDetails.getUsername().toUpperCase());
+		model.addAttribute("year",this.annio);
 
-		model.addAttribute("recibos", listFilesWindows("smb:\\\\192.168.0.29\\Usuarios\\STI\\Comprobantes Nomina\\"+annio+"\\", userDetails.getNumeroEmpleado()));
+		model.addAttribute("recibos", listFilesWindows("smb:\\\\192.168.0.29\\Usuarios\\STI\\Comprobantes Nomina\\"+this.annio+"\\", userDetails.getNumeroEmpleado()));
 		logger.info("Inicio de sesion usuario:" + userDetails.getUsername());
 
 		return "recibo";
@@ -57,7 +60,7 @@ public class ReciboController {
 			// Domain server verification
 			NtlmPasswordAuthentication auth = new NtlmPasswordAuthentication("tgm.com.mx", "garias", "buendia34");
 			SmbFile remoteFile = new SmbFile(shareDirectory,
-					"smb://192.168.0.29/Usuarios/STI/Comprobantes Nomina/2020/", auth);
+					"smb://192.168.0.29/Usuarios/STI/Comprobantes Nomina/"+this.annio+ "/", auth);
 			System.out.print(
 					"Remote shared directory access time-consuming: [{}]" + (System.currentTimeMillis() - startTime));
 			if (remoteFile.exists() && 	remoteFile.isDirectory()) {
